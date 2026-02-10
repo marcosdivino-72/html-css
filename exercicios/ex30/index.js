6bcf28a21bbb1fcb1c04f95677fa5a7ebc965051
marcosdivino-72 <marcosdivino999@gmail.com>
2026-02-10 19:36:01 -0300

    lista de tarefas atualizada

--- exercicios/ex30/index.js
+++ exercicios/ex30/index.js
@@ -9,25 +9,51 @@ class ListaDeTarefas{
     constructor(lista,btn){
         this.btn=btn;
         this.list = lista;
+       
+        if(localStorage.getItem("id")==null){
+            localStorage.setItem("id",0)
+             
+        }
+        this.id = localStorage.getItem("id")
+
         this.addBtn()
       
         
 
     }
-    criarCorpo(){
+    criarCorpo(id){
+        if(id!=null){
+            if(localStorage.getItem(id)==null){
+                return
+            }
+        }
         const li = document.createElement("li")
-        const box = document.createElement("input")
+      //  const box = document.createElement("input")
         const samp = document.createElement("samp")
         //Elementos para criar a lista adicionado
-        box.type="checkbox"
-   box.id="check";
-         li.append(box);
-       li.innerHTML+=this.addTarefas()
+      //  box.type="checkbox"
+  // box.id="check";
+    //     li.append(box);
+     if(id!=undefined){  li.innerHTML+=this.addTarefas(id)
+
+        li.id=id
+     }
+     else{
+         li.innerHTML+=this.addTarefas()
+         let id = localStorage.getItem("id")
+         li.id=id
+         this.addBancoDeDados(this.addTarefas(),+id)
+     }
         samp.textContent="X"
         li.append(samp)
         this.list.append(li)
  
        this.addEventoRemove()
+    }
+    addBancoDeDados(tarefa,id){
+        localStorage.setItem("id",+localStorage.getItem("id")+1)
+       
+        localStorage.setItem(id,tarefa)
     }
     addEventoRemove(){
         const samp = document.querySelectorAll('samp')
@@ -39,13 +65,22 @@ class ListaDeTarefas{
         })
 
     }
-    addTarefas(){
-        const tarefa = document.querySelector("#tarefa").value;
-       return  tarefa ;
+    addTarefas(id){
+        if(id!=undefined){ 
 
+            const e =localStorage.getItem(id)
+            return  e;
+         }
+     else{
+        const tarefa = document.querySelector("#tarefa").value;
+
+       return  tarefa ;
+     }
+     
   }
     remove_list(e){
-       e.target.parentElement.remove()    
+        localStorage.removeItem( e.target.parentElement.id)   
+       e.target.parentElement.remove() 
     }
     addBtn(){
         this.btn.addEventListener("click",()=>{
@@ -62,13 +97,29 @@ class ListaDeTarefas{
         );
       
     }
-
+   
 }
+
+
 
 l = new ListaDeTarefas(list,btn)
 
 
 l.addEventoRemove()
+
+window.addEventListener("load",()=>{
+
+    const id = localStorage.getItem("id")
+    
+    for(let i=1;i<id;i++){
+      
+        l.criarCorpo(i)
+       
+    }
+})
+
+
+
 
   
  
